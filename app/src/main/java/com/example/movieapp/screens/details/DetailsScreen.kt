67a569ca05ef938e6e134cf44ef251d880461a87
shmzzzz <slt0623@gmail.com>
2024-04-28 22:24:47 +0900

    feat: DetailsScreenで写真を表示できるようにする

--- app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
@@ -4,16 +4,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -24,12 +29,24 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.movieapp.model.Movie
+import com.example.movieapp.model.getMovies
+import com.example.movieapp.widgets.MovieRow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailsScreen(navController: NavController, movieData: String?) {
+fun DetailsScreen(
+    navController: NavController,
+    movieId: String?
+) {
+    val newMovieList = getMovies().filter { movie ->
+        movie.id == movieId
+    }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -60,18 +77,39 @@ fun DetailsScreen(navController: NavController, movieData: String?) {
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
+                verticalArrangement = Arrangement.Top,
             ) {
+                MovieRow(movie = newMovieList.first())
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
                 Text(
-                    text = movieData.toString(),
-                    style = MaterialTheme.typography.titleLarge
+                    text = "Movie Images",
                 )
-                Spacer(modifier = Modifier.height(24.dp))
-                Button(onClick = {
-                    navController.popBackStack()
-                }) {
-                    Text(text = "Go Back")
-                }
+                HorizontalScrollableImageView(newMovieList)
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun HorizontalScrollableImageView(newMovieList: List<Movie>) {
+    LazyRow {
+        items(newMovieList.first().images) { image ->
+            Card(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(240.dp),
+                elevation = CardDefaults.cardElevation(5.dp)
+            ) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(image)
+                        .build(),
+                    contentDescription = "Movie Image",
+                    modifier = Modifier.fillMaxSize(),
+                )
             }
         }
     }
